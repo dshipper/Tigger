cccbc5c0f7a89f67f22852682707108f35d0cedf
Dan Shipper <dshipper@gmail.com>
2011-07-06 22:40:34 -0400

    Fixed multiple commit issue

--- post_commit_hook.rb
+++ post_commit_hook.rb
@@ -16,7 +16,7 @@ completed_tasks = Array.new
 tigger.each_line { |line|  tasks << line }
 
 commit = commits.last
-
+ 
 tasks.each do | task |                                                      
     if (commit.message.strip.upcase.index(task.strip.upcase) != nil)
       completed_tasks << (Time.now.to_i.to_s + "<?TIG?>" + task)
